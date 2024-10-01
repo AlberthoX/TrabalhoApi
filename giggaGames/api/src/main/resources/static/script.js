1892da635b980dd9f5f849eed7bf580dbe7e5ded
Salis Silva <salis.silva@aluno.senai.br>
2024-10-01 18:12:08 -0300

    validacao inputs

--- giggaGames/api/src/main/resources/static/script.js
+++ giggaGames/api/src/main/resources/static/script.js
@@ -4,7 +4,6 @@ const modalContent = document.querySelector("#modalContentInfo");
 const modalContentImg = document.querySelector(".modal-content-imgs");
 const inputPesquisa = document.querySelector(".inputPesquisa");
 const content = document.querySelector(".content");
-const close = document.querySelector(".close");
 const modalAdicionar = document.querySelector("#modalAdicionar");
 const modalRemover = document.querySelector("#modalRemover");
 const modalLoading = document.querySelector("#modalLoading");
@@ -15,214 +14,189 @@ const botaoRemoverJogo = document.querySelector("#botaoRemoverJogo");
 const tabelaAllJogos = document.querySelector("#tabelaAllJogos");
 const inputRemoverJogo = document.querySelector("#inputRemoverJogo");
 
-
-
-
-
-
-close.addEventListener('click', (event)=>{
-    modal.style.display = 'none';
+botaoAdicionarJogo.addEventListener("click", (event) => {
+  modalAdicionar.style.display = "block";
+  window.onclick = function (event) {
+    if (event.target === modalAdicionar) {
+      modalAdicionar.style.display = "none";
+    }
+  };
 });
 
-
-
-botaoAdicionarJogo.addEventListener('click', (event)=>{
-    modalAdicionar.style.display = 'block';
-    window.onclick = function(event) {
-        if (event.target === modalAdicionar) {
-            modalAdicionar.style.display = "none";
-        }
-      }
-});
-
-botaoRemoverJogo.addEventListener('click', (event)=>{
-    modalRemover.style.display = 'block';
-    window.onclick = function(event) {
-        if (event.target === modalRemover) {
-            modalRemover.style.display = "none";
-        }
-      }
+botaoRemoverJogo.addEventListener("click", (event) => {
+  modalRemover.style.display = "block";
+  window.onclick = function (event) {
+    if (event.target === modalRemover) {
+      modalRemover.style.display = "none";
+    }
+  };
 });
 
 const optionsGET = {
-    method: "GET",
-}
+  method: "GET",
+};
 
 fetch("http://localhost:8080/giggagames/v1", optionsGET)
-.then(response => response.json())
-.then(data => {
-    console.log(data)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
 
     for (let index = 0; index < data.length; index++) {
-        let eachGame = document.createElement("div");
-        eachGame.className = "eachGame";
+      let eachGame = document.createElement("div");
+      eachGame.className = "eachGame";
 
-        let capaJogo = document.createElement("img");
-        let nomeJogo = document.createElement("h1");
-        let capaWidescreen = document.createElement("img");
-        let descricao = document.createElement("h2");
-        let plataforma = document.createElement("h3");
+      let capaJogo = document.createElement("img");
+      let nomeJogo = document.createElement("h1");
+      let capaWidescreen = document.createElement("img");
+      let descricao = document.createElement("h2");
+      let plataforma = document.createElement("h3");
 
-        capaJogo.src = data[index].capa;
-        capaJogo.addEventListener("click", () => {
-            modalContent.innerHTML = "";
+      capaJogo.src = data[index].capa;
+      capaJogo.addEventListener("click", () => {
+        modalContent.innerHTML = "";
 
+        window.onclick = function (event) {
+          if (event.target === modal) {
+            modal.style.display = "none";
+          }
+        };
 
-            window.onclick = function(event) {
-                if (event.target === modal) {
-                  modal.style.display = "none";
-                }
-            }
+        let divImgModal = document.createElement("div");
+        divImgModal.id = "divImgModal";
 
-            let divImgModal = document.createElement("div")
-            divImgModal.id = "divImgModal"
+        let divScreenshot = document.createElement("div");
+        divScreenshot.className = "divScreenshot";
 
-            let divScreenshot = document.createElement("div");
-            divScreenshot.className = "divScreenshot"
+        nomeJogo.textContent = data[index].nome;
+        modal.style.display = "flex";
+        capaWidescreen.src = data[index].capaWidescreen;
 
-            nomeJogo.textContent = data[index].nome;
-            modal.style.display = "flex";
-            capaWidescreen.src = data[index].capaWidescreen;
+        descricao.textContent = data[index].descricao;
+        plataforma.textContent = "Plataformas: " + data[index].plataforma;
 
-            descricao.textContent = data[index].descricao;
-            plataforma.textContent = "Plataformas: " + data[index].plataforma;
-    
-            modalContent.appendChild(nomeJogo)
-            divImgModal.appendChild(capaWidescreen)
-            modalContent.appendChild(divImgModal)
+        modalContent.appendChild(nomeJogo);
+        divImgModal.appendChild(capaWidescreen);
+        modalContent.appendChild(divImgModal);
 
-            for (let index2 = 0; index2 < data[index].screenshotsGame.length; index2++) {
-                let screenshots = document.createElement("img");
-                screenshots.className = "screenshots";
-                screenshots.src = data[index].screenshotsGame[index2];
-                divScreenshot.appendChild(screenshots);
-                modalContent.appendChild(divScreenshot);
-            }
-            modalContent.appendChild(descricao)
-            modalContent.appendChild(plataforma)
-        })
+        for (
+          let index2 = 0;
+          index2 < data[index].screenshotsGame.length;
+          index2++
+        ) {
+          let screenshots = document.createElement("img");
+          screenshots.className = "screenshots";
+          screenshots.src = data[index].screenshotsGame[index2];
+          divScreenshot.appendChild(screenshots);
+          modalContent.appendChild(divScreenshot);
+        }
+        modalContent.appendChild(descricao);
+        modalContent.appendChild(plataforma);
+      });
 
-        eachGame.appendChild(capaJogo);
-        games.appendChild(eachGame);
+      eachGame.appendChild(capaJogo);
+      games.appendChild(eachGame);
 
-        let tr = document.createElement("tr");
-        let tdId = document.createElement("td");
-        let tdNome = document.createElement("td");
+      let tr = document.createElement("tr");
+      let tdId = document.createElement("td");
+      let tdNome = document.createElement("td");
 
-        tdId.textContent = data[index].id
-        tdNome.textContent = data[index].nome
+      tdId.textContent = data[index].id;
+      tdNome.textContent = data[index].nome;
 
-
-
-        tr.appendChild(tdId)
-        tr.appendChild(tdNome)
-        tabelaAllJogos.appendChild(tr)
-
-        
-
+      tr.appendChild(tdId);
+      tr.appendChild(tdNome);
+      tabelaAllJogos.appendChild(tr);
     }
-})
-
+  });
 
 const botaoSalvar = document.querySelector(".botaoSalvar");
-const inputNomeJogo = document.querySelector("#inputNomeJogo")
-const inputDescricaoJogo = document.querySelector("#inputDescricaoJogo")
-const inputPlataformaJogo = document.querySelector("#inputPlataformaJogo")
-const inputPosterJogo = document.querySelector("#inputPosterJogo")
-const inputPrecoJogo = document.querySelector("#inputPrecoJogo")
-const inputScreenshot1Jogo = document.querySelector("#inputScreenshot1Jogo")
-const inputScreenshot2Jogo = document.querySelector("#inputScreenshot2Jogo")
-const imgCapa = document.querySelector("#imgCapa")
-
-
-
-
-const inputCapaJogo = document.querySelector("#inputCapaJogo")
-inputCapaJogo.addEventListener("change", uploadImg)
+const inputNomeJogo = document.querySelector("#inputNomeJogo");
+const inputDescricaoJogo = document.querySelector("#inputDescricaoJogo");
+const inputPlataformaJogo = document.querySelector("#inputPlataformaJogo");
+const inputPosterJogo = document.querySelector("#inputPosterJogo");
+const inputPrecoJogo = document.querySelector("#inputPrecoJogo");
+const inputScreenshot1Jogo = document.querySelector("#inputScreenshot1Jogo");
+const inputScreenshot2Jogo = document.querySelector("#inputScreenshot2Jogo");
+const imgCapa = document.querySelector("#imgCapa");
+const inputCapaJogo = document.querySelector("#inputCapaJogo");
+inputCapaJogo.addEventListener("change", uploadImg);
 
 function uploadImg() {
-    imgCapa.src = inputCapaJogo.value;
+  imgCapa.src = inputCapaJogo.value;
 }
 
+botaoSalvar.addEventListener("click", (event) => {
+  event.preventDefault();
 
-botaoSalvar.addEventListener('click', () => {
-    console.log(inputCapaJogo.value)
-    let jogoadicionado = {
-        nome: inputNomeJogo.value,
-        descricao: inputDescricaoJogo.value,
-        capa: inputCapaJogo.value,
-        capaWidescreen: inputPosterJogo.value,
-        preco: inputPrecoJogo.value,
-        plataforma: inputPlataformaJogo.value,
-        screenshotsGame: [
-            inputScreenshot1Jogo.value,
-            inputScreenshot2Jogo.value,
-        ]
-    }
-    console.log(JSON.stringify(jogoadicionado))
-    const optionsPOST = {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(jogoadicionado),
-    }
-    
+  if (
+    inputNomeJogo.value.trim() === "" ||
+    inputDescricaoJogo.value.trim() === "" ||
+    inputCapaJogo.value.trim() === "" ||
+    inputPosterJogo.value.trim() === "" ||
+    inputPrecoJogo.value.trim() === "" ||
+    inputPlataformaJogo.value.trim() === "" ||
+    inputScreenshot1Jogo.value.trim() === "" ||
+    inputScreenshot2Jogo.value.trim() === ""
+  ) {
+    alert("Por favor, preencha todos os campos antes de salvar.");
+    return;
+  }
 
-    fetch("http://localhost:8080/giggagames/v1", optionsPOST)
-    .then(response => response)
-    .then(data => {
-        console.log(data);
-    })
+  let jogoAdicionado = {
+    nome: inputNomeJogo.value,
+    descricao: inputDescricaoJogo.value,
+    capa: inputCapaJogo.value,
+    capaWidescreen: inputPosterJogo.value,
+    preco: inputPrecoJogo.value,
+    plataforma: inputPlataformaJogo.value,
+    screenshotsGame: [inputScreenshot1Jogo.value, inputScreenshot2Jogo.value],
+  };
 
-    modalLoading.style.display = "block"
-    modalAdicionar.style.display = "none";
+  console.log(JSON.stringify(jogoAdicionado));
 
-    setTimeout(() => {
-        window.location.reload();
-        modalLoading.style.display = "none"
-    }, 1500);
-    
+  const optionsPOST = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jogoAdicionado),
+  };
 
-})
+  fetch("http://localhost:8080/giggagames/v1", optionsPOST)
+    .then((response) => response)
+    .then((data) => {
+      console.log(data);
+    });
 
+  modalLoading.style.display = "block";
+  modalAdicionar.style.display = "none";
+
+  setTimeout(() => {
+    window.location.reload();
+    modalLoading.style.display = "none";
+  }, 1500);
+});
 
 const optionsDELETE = {
-    method: "DELETE",
-   
-}
+  method: "DELETE",
+};
 
-
-
-const botaoExcluir = document.querySelector("#botaoExcluir")
-
+const botaoExcluir = document.querySelector("#botaoExcluir");
 botaoExcluir.addEventListener("click", () => {
-    fetch("http://localhost:8080/giggagames/v1/"+ inputRemoverJogo.value, optionsDELETE)
-    .then(response => response)
-    .then(data => {
-        console.log(data);
-    })
+  fetch(
+    "http://localhost:8080/giggagames/v1/" + inputRemoverJogo.value,
+    optionsDELETE
+  )
+    .then((response) => response)
+    .then((data) => {
+      console.log(data);
+    });
 
-    modalRemover.style.display = "none"
-    modalLoading.style.display = "block"
+  modalRemover.style.display = "none";
+  modalLoading.style.display = "block";
 
-    setTimeout(() => {
-        window.location.reload();
-        modalLoading.style.display = "none"
-    }, 1500);
-    
-
-})
-
-
-
-
-
-  
-
-
-
-
-
-
-
+  setTimeout(() => {
+    window.location.reload();
+    modalLoading.style.display = "none";
+  }, 1500);
+});
